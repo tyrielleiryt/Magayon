e5c5822fec195bd69428b3d469d9bb533f9a8f6d
tyrielleiryt <gvt.itcasaptaytay@gmail.com>
2025-12-27 09:52:33 +0800

    main html

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-/* Firebase config */
+/* FIREBASE */
 const firebaseConfig = {
   apiKey: "AIzaSyAojoYbRWIPSEf3a-f5cfPbV-U97edveHg",
   authDomain: "magayon.firebaseapp.com",
@@ -16,20 +16,21 @@ const db = getFirestore(app);
 /* ADMIN GUARD */
 onAuthStateChanged(auth, async (user) => {
   if (!user) {
-    window.location.href = "index.html";
+    window.location.replace("index.html");
     return;
   }
 
   const snap = await getDoc(doc(db, "users", user.uid));
+
   if (!snap.exists() || snap.data().role !== "admin") {
-    window.location.href = "order.html";
+    window.location.replace("order.html");
   }
 });
 
 /* LOGOUT */
 document.getElementById("logoutBtn").addEventListener("click", () => {
   signOut(auth).then(() => {
-    window.location.href = "index.html";
+    window.location.replace("index.html");
   });
 });
 
@@ -49,3 +50,10 @@ function updateDateTime() {
 
 updateDateTime();
 setInterval(updateDateTime, 60000);
+
+/* SIDEBAR NAVIGATION */
+document.querySelectorAll(".nav-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    window.location.href = btn.dataset.page;
+  });
+});
