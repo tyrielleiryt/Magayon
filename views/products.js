cff54a0afeac4135e04de9c211fb4fe9c0506970
tyrielleiryt <gvt.itcasaptaytay@gmail.com>
2026-01-01 22:58:17 +0800

    Update products.js

--- views/products.js
+++ views/products.js
@@ -7,7 +7,7 @@ const API_GET_URL =
 
 /* 🔴 REPLACE WITH YOUR ACTUAL googleusercontent URL */
 const UPLOAD_URL =
-  "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLiYvhqVLo7bFZkb5xSNUOtP08zqeGAADLqX4bpGYAs4Lg9kjNcM1i1U5-vT-zC_yaRvdShr2g_6kRfTPL_E8PPnAHrYJdIu3emJt8nainRx1BUzTXrHnd2wG3Qs1EIxgdR3rNshL5qX9SBLUoDLPX6un1DE-4BYkfkG-gMMLzlDXS-82k8qWBdPsUkJfuYYfcVH55l8EFAsjlWDxLvCwybYSbEpiAZCFybPlIgIE2KK3M2HCzn0W7dyJCUaJclDrwOrINn4WpMCMKa_PjBv5y2qSItigLHk0dhCXTF93WWbHMLuvjc&lib=MJrHeO5UOtvZjUj-Ywv-pGGXl3gJVnX-m";
+  "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjsflMCaBM-BKlNAc2T0FiiWCLbohao1EbirSKvF6boGC6AaTcrVRaeISFVNwD1N7FsAxvW-UwOS_SVHCkECvLUWaV4Mdo33zq6qyBvnR0JE7RzqoHotsMUUpmHTFF761aKSBt9DoYRA4KPosOwKDoWPg6qN3JfsgdqU8UxbVurhnAJ6D2ReOo6Wrpo6a0FzRykFydQy-liN4oZkTkH3K-ymmNOndnWUqsv_luIPGN2jCQo4T3cTKCryr2HqCkJfDIDyHqRYlCHwrMZ8C-TfXWJgGQHYw&lib=MJrHeO5UOtvZjUj-Ywv-pGGXl3gJVnX-m";
 
 /* ================= STATE ================= */
 let productsCache = [];
